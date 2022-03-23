18ca123a326a152e992d9dc317fb1fb0c8c95de7
Hangsam <102114166+Hangsamkaiji@users.noreply.github.com>
2022-03-23 14:15:07 +1100

    Add files via upload

---
+++ task1/task1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblCumValue = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(201, 166);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 0;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(201, 253);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How old is the textbook?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "How much did you pay for it?";
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(514, 249);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnEnd.Size = new System.Drawing.Size(111, 23);
+            this.btnEnd.TabIndex = 4;
+            this.btnEnd.Text = "End the quote";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(517, 123);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 13);
+            this.lblValue.TabIndex = 5;
+            // 
+            // lblCumValue
+            // 
+            this.lblCumValue.AutoSize = true;
+            this.lblCumValue.Location = new System.Drawing.Point(517, 142);
+            this.lblCumValue.Name = "lblCumValue";
+            this.lblCumValue.Size = new System.Drawing.Size(0, 13);
+            this.lblCumValue.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(201, 293);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblCumValue);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtAge);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label lblCumValue;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ task1/task1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Hangsam Nembang SDV SAC TASK 1
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public float cumuValue = 0f;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        //End programme by clearing cumulative value
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cumuValue = 0f;
+            lblCumValue.Text = "";
+            lblValue.Text = "";
+
+
+        }
+        //calculates cumulative and singular value by taking age and purchased value as input. Accounts for non parseable values.
+        public void calculateValue()
+        {
+            float depreciation, currentValue;
+            if (float.TryParse(txtPrice.Text, out float purchasedValue) && float.TryParse(txtAge.Text, out float age))
+            {
+                depreciation = purchasedValue * 0.2f * age;
+                if (depreciation > purchasedValue)
+                {
+                    lblValue.Text = "It is worth $0";
+                }
+                else
+                {
+                    currentValue = purchasedValue - depreciation;
+                    lblValue.Text = "It is worth $" + currentValue;
+                    cumuValue = cumuValue + currentValue;
+                    lblCumValue.Text = "The collection so far is worth $" + cumuValue;
+                }
+
+
+            }
+            else
+            {
+                lblValue.Text = "Error, please enter a valid number!";
+            }
+
+        }
+       
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            calculateValue();
+            
+            txtAge.Clear();
+            txtPrice.Clear();
+
+
+        }
+    }
+}
