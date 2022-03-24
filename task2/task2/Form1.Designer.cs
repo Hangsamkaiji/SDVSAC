5804060bab76963286e212827ffc57adf7a2460e
Hangsam <102114166+Hangsamkaiji@users.noreply.github.com>
2022-03-24 12:15:42 +1100

    Add files via upload

---
+++ task2/task2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnU = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(36, 52);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(75, 23);
+            this.btnU.TabIndex = 0;
+            this.btnU.Text = "Upload";
+            this.btnU.UseVisualStyleBackColor = true;
+            this.btnU.Click += new System.EventHandler(this.btnU_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(36, 106);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1059, 478);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1140, 636);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnU);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnU;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ task2/task2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Hangsam Nembang Year 12 SAC 1 TASK 2
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        List<string> lines = new List<string>();
+        double _totalProfit = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            BindingSource bs = new BindingSource();
+            
+            
+
+            
+
+        }
+        //Inputs an array of string and creates a header out of the first line of the CSV file.
+        public void setHeader(string[] _headers)
+        {
+            int counter = 0;
+            dataGridView2.ColumnCount = _headers.Length + 1;
+            for (int i = 0; i < _headers.Length; i++)
+            {
+                dataGridView2.Columns[i].HeaderText = _headers[i];
+                counter = i + 1;
+
+            }
+            dataGridView2.Columns[counter].HeaderText = "Profit";
+
+        }
+
+        //Takes CSV files as input, splits the CSV file text by commas and assigns it to appropriate variables. Outputs profit and prints csv file into datagrid
+        public void readCSV()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                double profit, purchasePrice;
+                string salePrice;
+                
+
+                string filepath = openFileDialog.FileName;
+                List<string> tempList = new List<string>();
+                String[] headers = File.ReadAllLines(filepath).First().Split(',').ToArray();
+                setHeader(headers);
+                lines = File.ReadAllLines(filepath).Skip(1).ToList();
+                foreach (var line in lines)
+                {
+                    tempList = line.Split(',').ToList();
+                    purchasePrice = double.Parse(tempList[4]);
+                    salePrice = tempList[5];
+                    if(salePrice == "NA")
+                    {
+                        profit = purchasePrice * -1;
+                    }
+                    else
+                    {
+                        profit = double.Parse(salePrice) - purchasePrice;
+                    }
+                    tempList.Add(profit.ToString());
+                    dataGridView2.Rows.Add(tempList.ToArray());
+                    _totalProfit = _totalProfit + profit;
+
+
+
+
+
+
+                }
+
+                dataGridView2.Rows.Add("", "", "", "", "", "Total Profit", "$" + _totalProfit.ToString());
+
+            }
+
+            
+
+           
+
+        }
+
+
+        private void btnU_Click(object sender, EventArgs e)
+        {
+            readCSV();
+        }
+    }
+}
